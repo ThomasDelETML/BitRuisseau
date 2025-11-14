217e1234d0f78b3854a16413d5f2c77b1baa3e7b
Thomas <163824101+ThomasDelETML@users.noreply.github.com>
2025-11-14 11:24:41 +0100

    feat (code) : Rajout des grids et logique
    [90][WIP]

--- code/BitRuisseau/Form1.Designer.cs
+++ code/BitRuisseau/Form1.Designer.cs
@@ -28,18 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnSelectFolder = new Button();
+            lblFolder = new Label();
+            dgvLocalSongs = new DataGridView();
+            btnRefreshMediatheques = new Button();
+            lstMediatheques = new ListBox();
+            dgvRemoteSongs = new DataGridView();
+            btnImportSong = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvLocalSongs).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvRemoteSongs).BeginInit();
             SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            btnSelectFolder.Location = new Point(172, 12);
+            btnSelectFolder.Name = "btnSelectFolder";
+            btnSelectFolder.Size = new Size(75, 23);
+            btnSelectFolder.TabIndex = 0;
+            btnSelectFolder.Text = "btn";
+            btnSelectFolder.UseVisualStyleBackColor = true;
+            // 
+            // lblFolder
+            // 
+            lblFolder.AutoSize = true;
+            lblFolder.Location = new Point(23, 16);
+            lblFolder.Name = "lblFolder";
+            lblFolder.Size = new Size(20, 15);
+            lblFolder.TabIndex = 1;
+            lblFolder.Text = "lbl";
+            // 
+            // dgvLocalSongs
+            // 
+            dgvLocalSongs.AllowUserToAddRows = false;
+            dgvLocalSongs.AllowUserToDeleteRows = false;
+            dgvLocalSongs.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvLocalSongs.Location = new Point(23, 110);
+            dgvLocalSongs.Name = "dgvLocalSongs";
+            dgvLocalSongs.ReadOnly = true;
+            dgvLocalSongs.Size = new Size(240, 150);
+            dgvLocalSongs.TabIndex = 2;
+            dgvLocalSongs.CellContentClick += dgvLocalSongs_CellContentClick;
+            // 
+            // btnRefreshMediatheques
+            // 
+            btnRefreshMediatheques.Location = new Point(590, 46);
+            btnRefreshMediatheques.Name = "btnRefreshMediatheques";
+            btnRefreshMediatheques.Size = new Size(75, 23);
+            btnRefreshMediatheques.TabIndex = 3;
+            btnRefreshMediatheques.Text = "btn";
+            btnRefreshMediatheques.UseVisualStyleBackColor = true;
+            // 
+            // lstMediatheques
+            // 
+            lstMediatheques.FormattingEnabled = true;
+            lstMediatheques.ItemHeight = 15;
+            lstMediatheques.Location = new Point(518, 93);
+            lstMediatheques.Name = "lstMediatheques";
+            lstMediatheques.Size = new Size(120, 94);
+            lstMediatheques.TabIndex = 4;
+            // 
+            // dgvRemoteSongs
+            // 
+            dgvRemoteSongs.AllowUserToAddRows = false;
+            dgvRemoteSongs.AllowUserToDeleteRows = false;
+            dgvRemoteSongs.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvRemoteSongs.Location = new Point(492, 250);
+            dgvRemoteSongs.Name = "dgvRemoteSongs";
+            dgvRemoteSongs.ReadOnly = true;
+            dgvRemoteSongs.Size = new Size(240, 150);
+            dgvRemoteSongs.TabIndex = 5;
+            // 
+            // btnImportSong
+            // 
+            btnImportSong.Location = new Point(410, 185);
+            btnImportSong.Name = "btnImportSong";
+            btnImportSong.Size = new Size(75, 23);
+            btnImportSong.TabIndex = 6;
+            btnImportSong.Text = "btn";
+            btnImportSong.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnImportSong);
+            Controls.Add(dgvRemoteSongs);
+            Controls.Add(lstMediatheques);
+            Controls.Add(btnRefreshMediatheques);
+            Controls.Add(dgvLocalSongs);
+            Controls.Add(lblFolder);
+            Controls.Add(btnSelectFolder);
             Name = "MainForm";
             Text = "BitRuisseau";
+            ((System.ComponentModel.ISupportInitialize)dgvLocalSongs).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvRemoteSongs).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnSelectFolder;
+        private Label lblFolder;
+        private DataGridView dgvLocalSongs;
+        private Button btnRefreshMediatheques;
+        private ListBox lstMediatheques;
+        private DataGridView dgvRemoteSongs;
+        private Button btnImportSong;
     }
 }
